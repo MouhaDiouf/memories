ab7e0b7801fd6d037d2af141e672279138d990f8
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-15 13:04:14 +0000

    Create the posts reducer

--- client/src/index.js
+++ client/src/index.js
@@ -4,5 +4,8 @@ import { Provider } from 'react';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import App from './App';
+import reducers from './reducers/index';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(<App />, document.getElementById('root'));
