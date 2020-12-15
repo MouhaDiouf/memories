2ba675b00e4354d2574836a3e8f3684b89b9755a
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-15 14:41:38 +0000

    Install material-ui/icons

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import useStyles from './styles';
+import {
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Button,
+  Typography,
+} from '@material-ui/core';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
 function Post() {
   const classes = useStyles();
   return (
