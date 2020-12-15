c55f81f4857002a5776208ea35ba4e7e4bc19c9c
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-15 14:38:35 +0000

    Create the logic for showing posts

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import Post from './Post/Post';
 import useStyles from './styles';
 import { useSelector } from 'react-redux';
@@ -7,12 +8,22 @@ function Posts() {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
   console.log(posts);
+  if (!posts.length) {
+    return <CircularProgress />;
+  }
   return (
-    <>
-      <h1>Posts</h1>
-      <Post />
-      <Post />
-    </>
+    <Grid
+      className={classes.container}
+      container
+      alignItems="stretch"
+      spacing={3}
+    >
+      {posts.map((post) => (
+        <Grid key={post.id} item xs={12} sm={6}>
+          <Post post={post} />
+        </Grid>
+      ))}
+    </Grid>
   );
 }
 
